86d7207458decddd95f3e8375f5d9231b04378d3
stijn-castelyns <stijn@u2u.be>
2026-07-24 15:10:45 +0200

    Bump to Version 1.1

--- src/SimpleWebApp/Program.cs
+++ src/SimpleWebApp/Program.cs
@@ -7,7 +7,7 @@ public class Program
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
 
-    app.MapGet("/", () => "Version 1.0");
+    app.MapGet("/", () => "Version 1.1");
 
     app.Run();
   }
